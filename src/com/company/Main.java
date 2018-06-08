5dbc3c90cab0cae025c395c5dc2480b558b67863
Carlos Sansoulet <telsen2@hotmail.com>
2018-06-08 09:53:22 -0400

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class fizzBuzzLogic() {
+    }
+
+public class createArrayCall() {
+        }
+
+public class randNumGenCall() {
+        }
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println(" Please enter a number form 1 to 99 "); //prompt to user
+        input = sc.nextLine();    									//get input from the user
+
+        fizzBuzzLogicCall();      	// call to FizBuzz logic
+
+        createArrayCall();     		// call a sub-program to create array for 100 elements
+
+        randomNumGenCall();         // call to create a random generator ->(BONUS points)
+
+        System.out.println("Fizz" + fizzNum);
+        System.out.println("Buzz" + buzzNum);
+
+
     }
 }
